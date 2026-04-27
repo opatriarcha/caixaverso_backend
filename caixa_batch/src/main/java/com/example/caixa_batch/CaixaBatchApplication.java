27bbf4d1ccc55af22f02beba584bd35052f1cda8
Orlando <orlando.patriarcha@gmail.com>
2026-04-27 14:43:17 -0300

    inicio projeto batch

---
+++ caixa_batch/src/main/java/com/example/caixa_batch/CaixaBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.caixa_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaBatchApplication.class, args);
+	}
+
+}
